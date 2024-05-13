8f42eda495b3db5082905c3cd405764b49acaf50
amy4522 <154403665+amy4522@users.noreply.github.com>
2024-05-13 15:35:52 +0800

    Add files via upload

---
+++ hello world.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	////printf("hello, world!\n");
+	//int num1 = 0;
+	//int num2 = 0;
+
+	////输入2个整数
+	//scanf("%d %d", &num1, &num2);
+	//
+	////求和
+	//int sum = num1 + num2;
+
+	////输出
+	//printf("%d\n", sum);
+
+	//return 0;
+	printf("ab\rcd\n\n");
+	printf("ab\ncd");
+}
