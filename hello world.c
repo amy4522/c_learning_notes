18de754965aebd579702ee8957f3de844ba02aaa
amy4522 <154403665+amy4522@users.noreply.github.com>
2024-05-13 15:37:13 +0800

    Update hello world.c

--- hello world.c
+++ hello world.c
@@ -3,20 +3,5 @@
 
 int main()
 {
-	////printf("hello, world!\n");
-	//int num1 = 0;
-	//int num2 = 0;
-
-	////输入2个整数
-	//scanf("%d %d", &num1, &num2);
-	//
-	////求和
-	//int sum = num1 + num2;
-
-	////输出
-	//printf("%d\n", sum);
-
-	//return 0;
-	printf("ab\rcd\n\n");
-	printf("ab\ncd");
+	printf("hello, world!\n");
 }
